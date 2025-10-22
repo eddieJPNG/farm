90cfd93a022c901c4c68a4de043c182d52f71a00
Edson <rochaedsonn12@gmail.com>
2025-10-22 09:07:43 -0300

    Adicionei o main.py e organizei as pastas que estavam trocadas

---
+++ src/main.py
@@ -0,0 +1,68 @@
+import os 
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def menu():
+    while True:
+
+        clear_screen()
+        print("\nSISTEMA DE FAZENDA DIGITAL EM  PHYTON")
+        print("1.Animais")
+        print("2.Plantações")
+        print("3.Insumos")
+        print("4.Movimentos")
+        print("5.Relatórios")
+        print("0.Sair")
+
+        option = input("Escolha uma das opções: ")
+
+        match option:
+            case "1":
+                animals_menu()
+            case "2":
+                plants_menu()
+            case "3":
+                inputs_menu()
+            case "4":
+                movements_menu()
+            case "5":
+                reports_menu()
+            case "0":
+                print("Saindo...")
+                break
+        
+
+
+
+    
+
+        
+
+
+
+        # if option == "1":
+        #     animals_menu()
+        # elif option == "2":
+        #     plants_menu()
+        # elif option == "3":
+        #     inputs_menu()
+        # elif option == "4":
+        #     moviments_menu()
+        # elif option == "5":
+        #     reports_menu()
+        # elif option == "0":
+        #     print("Saindo...")
+            
+           
+        #     break
+        # else:
+            # print("Opção invalida!")
+            # input("Pressione ENTER para tentar novamente!")
+           
+if __name__ == "__main__":
+    menu()
+
+option = input()
+
