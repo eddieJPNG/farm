df830ce998f05745ea0f6420f6e3c57f323cdf36
Edson <rochaedsonn12@gmail.com>
2025-10-23 11:40:52 -0300

    Alterei o report.py com formatação

---
+++ src/reports.py
@@ -0,0 +1,14 @@
+from files import *
+
+def generate_report():
+    print("\n RELATÓRIOS GERAIS DA FAZENDA")
+    relatorio = {
+        "animais": load_json("animais.json"),
+        "plantas": load_json("plants.json"),
+        "insumos": load_json("inputs.json"),
+        
+
+
+
+
+    }
