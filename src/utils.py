8d5d06baebf2e1421efcd3a635dc1bd24ffe2ea3
Edson <rochaedsonn12@gmail.com>
2025-10-27 12:17:42 -0300

    Mini-update

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,5 @@
 import os 
 from files import *
-from main import *
-from plants import *
-from inputs import *
-from animals import *
 import random 
 
 
@@ -11,27 +7,6 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def generate_plant_id(data):
-    # Gera ID no formato P001, P002... tenta respeitar IDs existentes (str ou int)
-    max_n = 0
-    for item in data:
-        raw = item.get("id")
-        if raw is None:
-            continue
-        s = str(raw)
-        if s.upper().startswith("P"):
-            try:
-                n = int(s[1:])
-            except:
-                continue
-        else:
-            try:
-                n = int(s)
-            except:
-                continue
-        if n > max_n:
-            max_n = n
-    return f"P{max_n+1:03d}"
 
 
 
